a3b14ebd7358162aeda06e6e48d4f251b041f0fa
Matej <matej.kosec@hotmail.com>
2018-06-24 13:06:18 -0700

    Fixed indentation

--- 3D.py
+++ 3D.py
@@ -7,56 +7,57 @@ import numpy as np
 import functools
 
 #%% Constants
-N_TIME = 125
+N_TIME = 80
 #file 1 has 663 timesteps
 #file 2 has 628 timesteps
-N_HIDDEN = 30
+N_HIDDEN = 40
 N_INPUT = 6
-N_PLOTS = 25
+N_PLOTS = 15
 N_OUTPUT = 6
-LR_BASE = (2e-3)#*0.99**100
-BATCH_SIZE = 60
-ITRS = 80 #1000
+LR_BASE = (1e-3)#*0.99**100
+BATCH_SIZE = 75 
+ITRS = 1000
 REG = 1.5e-2
-DROPOUT1= 0.1
-DROPOUT2= 0.1
+DROPOUT1= 0.05
+DROPOUT2= 0.05
 DECAY = 0.99
 RESTORE_CHECKPOINT  = False
 PERFORM_TRAINING = True
 SAVE_DIR = './checkpoints'
 
 def read_file(file_name):
-	with open(file_name,"r") as data_file:
-        	#lines = data_file.readlines()
-        	line = data_file.readline()
-        	row = 0
-        	x = 0
-        	all_data = []
-        	for line in data_file:
-        		numbers = line.split(',')
-        		#ts, t_diffs, xs,ys,zs,vxs,vys,vzs, x_ts,y_ts,z_ts,vx_ts,vy_ts,vz_ts = [each_line for each_line in numbers]
-        		line_items = [each_line for each_line in numbers]
-        		data = []
-        		for ino,item in enumerate(line_items):
-        			if ino == len(line_items):
-        				data.append(float(item[:-2].strip()))
-        				break
-        			data.append(float(item.strip()))
-        		all_data.append(data)
-        		row = row+1
-        	#print(row)
-	
-	data_reduced = np.vstack(all_data)[:,:]
-	data_reduced[:,2:5] = data_reduced[:,2:5] - data_reduced[1,2:5]
-	data_reduced[:,8:11] = data_reduced[:,8:11] - data_reduced[1,8:11]
-	
-	#Slicing data to get more training data for the dense layers
-	slice_size = 125
-	sliced_data = []
-	for i in range(row//slice_size):
-		sliced_data.append(np.array(data_reduced[i*slice_size:(i+1)*slice_size,:]))
-	
-	return sliced_data
+    with open(file_name,"r") as data_file:
+            #lines = data_file.readlines()
+            line = data_file.readline()
+            row = 0
+            x = 0
+            all_data = []
+            for line in data_file:
+                numbers = line.split(',')
+                #ts, t_diffs, xs,ys,zs,vxs,vys,vzs, x_ts,y_ts,z_ts,vx_ts,vy_ts,vz_ts = [each_line for each_line in numbers]
+                line_items = [each_line for each_line in numbers]
+                data = []
+                for ino,item in enumerate(line_items):
+                    if ino == len(line_items):
+                        data.append(float(item[:-2].strip()))
+                        break
+                    data.append(float(item.strip()))
+                all_data.append(data)
+                row = row+1
+            #print(row)
+    
+    data_reduced = np.vstack(all_data)[:,:]
+    data_reduced[:,2:5] = data_reduced[:,2:5] - data_reduced[1,2:5]
+    data_reduced[:,8:11] = data_reduced[:,8:11] - data_reduced[1,8:11]
+    data_reduced[:,2:8] *= -1
+    
+    #Slicing data to get more training data for the dense layers
+    slice_size = N_TIME
+    sliced_data = []
+    for i in range(row//slice_size):
+        sliced_data.append(np.array(data_reduced[i*slice_size:(i+1)*slice_size,:]))
+    
+    return sliced_data
 
 
 #%%
@@ -78,7 +79,7 @@ with g1.as_default():
     lstm_cell =tf.nn.rnn_cell.LSTMCell(N_HIDDEN,forget_bias=0.99)
     
     #Residual weapper
-    #lstm_cell = tf.nn.rnn_cell.ResidualWrapper(lstm_cell)    
+    lstm_cell = tf.nn.rnn_cell.ResidualWrapper(lstm_cell)    
     
     #Dropout wrapper
     lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, input_keep_prob=tf.maximum(1-DROPOUT2,1-tf.cast(is_training,tf.float32)),\
@@ -116,10 +117,10 @@ with g1.as_default():
 data = []
 #Add saver
 for i in range(10):
-	file_name = './NewData/Oval_circ1_N'+str(i+1)+'.txt'
-	data.extend(read_file(file_name))
-	file_name = './NewData/Oval_circ2_N'+str(i+1)+'.txt'
-	data.extend(read_file(file_name))
+    file_name = './NewData/Oval_circ1_N'+str(i+1)+'.txt'
+    data.extend(read_file(file_name))
+    file_name = './NewData/Oval_circ2_N'+str(i+1)+'.txt'
+    data.extend(read_file(file_name))
 #data contains [t, t_diff, x,y,z,vx,vy,vz, x_t,y_t,z_t,vx_t,vy_t,vz_t]
 
 all_data = sp.stack(data)
@@ -207,80 +208,80 @@ print(xk_batch.shape)
  
     #%%
 for i in range(test_batch_x.shape[0]):
-	print(i)
-	plt.figure(figsize=(14,4))
-	plt.subplot(231)
-	plt.title('Training progress ylog plot')
-	plt.gca().set_yscale('log')
-	plt.plot(range(0,ITRS,20),dev_loss_plot,label='dev loss')
-	plt.plot(range(0,ITRS,20),tra_loss_plot,label='train loss')
-	plt.xlabel('Adam iteration')
-	plt.ylabel('L2 fitting loss')
-	plt.grid(which='both')
-	plt.legend()
-	
-	plt.subplot(232)
-	plt.title('Position Horizontal')
-	plt.plot(test_batch_y[i,:,0],test_batch_y[i,:,1],label='True')
-	plt.plot(test_batch_x[i,:,0],test_batch_x[i,:,1],label='Measured')
-	plt.plot(xk_batch[i,:,0],xk_batch[i,:,1],label='Linear KF')
-	plt.plot(out2[i,:,0],out2[i,:,1],label='LSTM')
-	plt.axis('equal')
-	plt.xlabel('x axis')
-	plt.ylabel('y axis')
-	plt.grid(which='both')
-	plt.legend()
-	
-	plt.subplot(233)
-	plt.title('Position Vertical')
-	plt.plot(test_batch_y[i,:,2],label='True')
-	plt.plot(test_batch_x[i,:,2],label='Measured')
-	plt.plot(xk_batch[i,:,2],label='Linear KF')
-	plt.plot(out2[i,:,2],label='LSTM')
-	plt.xlabel('x axis')
-	plt.ylabel('y axis')
-	plt.grid(which='both')
-	plt.legend()
-	
-	plt.subplot(234)
-	plt.title('Velocity x')
-	plt.plot(test_batch_y[i,:,3],label='True')
-	plt.plot(test_batch_x[i,:,3],label='Measured')
-	plt.plot(xk_batch[i,:,3],label='Linear KF')
-	plt.plot(out2[i,:,3],label='LSTM')
-	plt.xlabel('x axis')
-	plt.ylabel('y axis')
-	plt.grid(which='both')
-	plt.legend()
-	
-	plt.subplot(235)
-	plt.title('Velocity y')
-	plt.plot(test_batch_y[i,:,4],label='True')
-	plt.plot(test_batch_x[i,:,4],label='Measured')
-	plt.plot(xk_batch[i,:,4],label='Linear KF')
-	plt.plot(out2[i,:,4],label='LSTM')
-	plt.xlabel('x axis')
-	plt.ylabel('y axis')
-	plt.grid(which='both')
-	plt.legend()
-	
-	plt.subplot(236)
-	plt.title('Velocity z')
-	plt.plot(test_batch_y[i,:,5],label='True')
-	plt.plot(test_batch_x[i,:,5],label='Measured')
-	plt.plot(xk_batch[i,:,5],label='Linear KF')
-	plt.plot(out2[i,:,5],label='LSTM')
-	plt.xlabel('x axis')
-	plt.ylabel('y axis')
-	plt.grid(which='both')
-	plt.legend()
-	
-	#plt.subplot(23)
-	#plt.title('Learning Rate')
-	#plt.gca().set_yscale('log')
-	#plt.plot(range(len(lr_plot)),lr_plot,label='Exponentially decayed to %i percent every 20 iterations'%(DECAY*100))
-	#plt.xlabel('Adam iteration')
-	#plt.ylabel('L2 fitting loss')
-	#plt.grid(which='both')
-	#plt.legend()
-	plt.savefig('training_progress2D'+str(i)+'.png',bbox_inches='tight', dpi=200)
+    print(i)
+    plt.figure(figsize=(14,4))
+    plt.subplot(231)
+    plt.title('Training progress ylog plot')
+    plt.gca().set_yscale('log')
+    plt.plot(range(0,ITRS,20),dev_loss_plot,label='dev loss')
+    plt.plot(range(0,ITRS,20),tra_loss_plot,label='train loss')
+    plt.xlabel('Adam iteration')
+    plt.ylabel('L2 fitting loss')
+    plt.grid(which='both')
+    plt.legend()
+    
+    plt.subplot(232)
+    plt.title('Position Horizontal')
+    plt.plot(test_batch_y[i,:,0],test_batch_y[i,:,1],label='True')
+    plt.plot(test_batch_x[i,:,0],test_batch_x[i,:,1],label='Measured')
+    plt.plot(xk_batch[i,:,0],xk_batch[i,:,1],label='Linear KF')
+    plt.plot(out2[i,:,0],out2[i,:,1],label='LSTM')
+    plt.axis('equal')
+    plt.xlabel('x axis')
+    plt.ylabel('y axis')
+    plt.grid(which='both')
+    plt.legend()
+    
+    plt.subplot(233)
+    plt.title('Position Vertical')
+    plt.plot(test_batch_y[i,:,2],label='True')
+    plt.plot(test_batch_x[i,:,2],label='Measured')
+    plt.plot(xk_batch[i,:,2],label='Linear KF')
+    plt.plot(out2[i,:,2],label='LSTM')
+    plt.xlabel('x axis')
+    plt.ylabel('y axis')
+    plt.grid(which='both')
+    plt.legend()
+    
+    plt.subplot(234)
+    plt.title('Velocity x')
+    plt.plot(test_batch_y[i,:,3],label='True')
+    plt.plot(test_batch_x[i,:,3],label='Measured')
+    plt.plot(xk_batch[i,:,3],label='Linear KF')
+    plt.plot(out2[i,:,3],label='LSTM')
+    plt.xlabel('x axis')
+    plt.ylabel('y axis')
+    plt.grid(which='both')
+    plt.legend()
+    
+    plt.subplot(235)
+    plt.title('Velocity y')
+    plt.plot(test_batch_y[i,:,4],label='True')
+    plt.plot(test_batch_x[i,:,4],label='Measured')
+    plt.plot(xk_batch[i,:,4],label='Linear KF')
+    plt.plot(out2[i,:,4],label='LSTM')
+    plt.xlabel('x axis')
+    plt.ylabel('y axis')
+    plt.grid(which='both')
+    plt.legend()
+    
+    plt.subplot(236)
+    plt.title('Velocity z')
+    plt.plot(test_batch_y[i,:,5],label='True')
+    plt.plot(test_batch_x[i,:,5],label='Measured')
+    plt.plot(xk_batch[i,:,5],label='Linear KF')
+    plt.plot(out2[i,:,5],label='LSTM')
+    plt.xlabel('x axis')
+    plt.ylabel('y axis')
+    plt.grid(which='both')
+    plt.legend()
+    
+    #plt.subplot(23)
+    #plt.title('Learning Rate')
+    #plt.gca().set_yscale('log')
+    #plt.plot(range(len(lr_plot)),lr_plot,label='Exponentially decayed to %i percent every 20 iterations'%(DECAY*100))
+    #plt.xlabel('Adam iteration')
+    #plt.ylabel('L2 fitting loss')
+    #plt.grid(which='both')
+    #plt.legend()
+    plt.savefig('training_progress2D'+str(i)+'.png',bbox_inches='tight', dpi=200)
